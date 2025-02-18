a52257c0436b006b0905f7772d290621f5044ffd
programmerShoaib <link2shoaib.2002@gmail.com>
2025-02-19 01:14:05 +0500

    this is the last commit

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
-    let playerScore = 0;
-    let computerScore = 0;                                              // initial scores
+    let playerScore = 1;
+    let computerScore = 1;                                              // initial scores
 
     let choices = document.querySelectorAll('.choice');                 // get all the choices
     let resultDisplay = document.querySelector('#msg');                 // get the result display
@@ -13,9 +13,9 @@
     });
 
     const playGame = (playerChoice) => {                                // this function get the player choice
-        console.log("this is player choice " , playerChoice);
+        // console.log("this is player choice " , playerChoice);
         const computerChoice = computerChoiceFunc();
-        console.log("this is computer choice " , computerChoice);
+        // console.log("this is computer choice " , computerChoice);
         decideWin(playerChoice, computerChoice);
     }
 
@@ -25,21 +25,21 @@
         return options[randIdx];
     }
 
-    const decideWin = (playerChoice, computerChoice) => {
+    const decideWin = (playerChoice, computerChoice) => {               // this function decide the winner
         let playerWin = true;                                           // player value is false initially
         if (playerChoice === computerChoice) {                          // if the player choice is equal to the computer choice
-            console.log("it's a tie");
+            // console.log("it's a tie");
             gameDraw(playerChoice, computerChoice);
         } else {
             if (playerChoice === 'rock') {                              // if the player choice is rock
                 playerWin = computerChoice === 'scissor' ? true : false;
-                console.log(playerWin);                                
+                // console.log(playerWin);                                
             } else if (playerChoice === 'paper') {                      // if the player choice is paper
                 playerWin = computerChoice === 'rock' ? true : false;
-                console.log(playerWin);
+                // console.log(playerWin);
             } else if (playerChoice === 'scissor') {                    // if the player choice is scissor
                 playerWin = computerChoice === 'paper' ? true : false;
-                console.log(playerWin);
+                // console.log(playerWin);
             }else {
                 console.log("something went wrong");
             }
@@ -52,21 +52,19 @@
             document.getElementById('playerScore').textContent = playerScore ++;                            // player score increment
             resultDisplay.textContent = `You win! ${playerChoice} beats ${computerChoice}`;                 // show the result
             document.getElementById(playerChoice).classList.add('green-glow');                              // add the green glow to the player choice
-            setTimeout(() => document.getElementById(playerChoice).classList.remove('green-glow'), 500);    // remove the green glow after 500ms
+            setTimeout(() => document.getElementById(playerChoice).classList.remove('green-glow'), 400);    // remove the green glow after 500ms
         } else {
             document.getElementById('compScore').textContent = computerScore ++;                            // computer score increment
             resultDisplay.textContent = `You lose! ${computerChoice} beats ${playerChoice}`;                // show the result
             document.getElementById(computerChoice).classList.add('red-glow');                              // add the red glow to the computer choice
-            setTimeout(() => document.getElementById(computerChoice).classList.remove('red-glow'), 500);    // remove the red glow after 500ms
+            setTimeout(() => document.getElementById(computerChoice).classList.remove('red-glow'), 400);    // remove the red glow after 500ms
         }
     }
 
     const gameDraw = (playerChoice, computerChoice) => {                                                  // this function show the draw condition 
         resultDisplay.textContent = "It's a draw!";
         document.getElementById(playerChoice).classList.add('gray-glow');
-        document.getElementById(computerChoice).classList.add('gray-glow');
         setTimeout(() => {
             document.getElementById(playerChoice).classList.remove('gray-glow');
-            document.getElementById(computerChoice).classList.remove('gray-glow');
         }, 1000);
     }
